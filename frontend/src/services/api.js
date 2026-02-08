797264ec4503bbc55f3b7864371572c8d5b68c0e
Shajana_Mirsha <shajanamirsha13@gmail.com>
2026-02-09 00:00:56 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://savingsgoaltracker.onrender.com/api"
+  baseURL: "https://savingsgoaltracker.onrender.com/api",
 
 });
 
