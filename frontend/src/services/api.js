42dd3f1c5f5d028738c4a55de32396b3f1717ff6
Shajana_Mirsha <shajanamirsha13@gmail.com>
2026-02-08 23:57:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL + "/api"
+  baseURL: "https://savingsgoaltracker.onrender.com/api"
+
 });
 
 // AUTH
