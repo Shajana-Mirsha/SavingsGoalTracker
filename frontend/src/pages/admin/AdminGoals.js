271121812e080f670c030062a8e49e5e18e005ea
Shajana_Mirsha <shajanamirsha13@gmail.com>
2026-03-29 18:47:40 +0530

    Update AdminGoals.js

--- frontend/src/pages/admin/AdminGoals.js
+++ frontend/src/pages/admin/AdminGoals.js
@@ -18,7 +18,7 @@ const AdminGoals = () => {
     const fetchGoals = async () => {
         try {
             const token = localStorage.getItem("token");
-            const res = await axios.get("http://localhost:5000/api/admin/goals", {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL || "http://localhost:5000/api"}/admin/goals`, {
                 headers: { Authorization: `Bearer ${token}` }
             });
             setGoals(res.data);
